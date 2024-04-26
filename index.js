2836766414430a8b814fad406a14ce2ee384879f
YashGoswami143 <yashgoswami1010@gmail.com>
2024-04-26 15:50:42 -0400

    isvalid() function added.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const age = 23;
-const username= "AcheivedYG";
+const user_name= "AcheivedYG";
 const email = "Yg@gmail.com";
 
 function get_username() {
- return username;
+ return user_name;
+}
+
+function isvalid(age) {
+    if (age >18){
+        return 1;
+    }
 }
